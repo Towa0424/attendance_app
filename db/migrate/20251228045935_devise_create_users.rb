eeed1c3ce4a251e5ba5eeeaec3bcaffe92a0a715
Towa0424 <w8d42bed72xbyx@gmail.com>
2026-01-13 23:36:30 +0900

    マイグレーション修正

--- db/migrate/20251228045935_devise_create_users.rb
+++ db/migrate/20251228045935_devise_create_users.rb
@@ -1,19 +1,19 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :email,               null: false, default: ""
+      t.string :email,               null: false, default: "", limit: 191
       t.string :encrypted_password,  null: false, default: ""
       t.string :name,                null: false
       t.integer :role,               null: false, default: 0
       t.references :group,           foreign_key: true           
-      t.string :employee_number,     null: false
+      t.string :employee_number,     null: false, limit: 50
       t.date :joined_on,             null: false
       t.date :retired_on
       t.integer :employment_type, null: false, default: 0
       t.integer :position,        null: false, default: 2
       t.integer :salary_type,     null: false, default: 0
       t.integer :salary_amount,      null: false
-      t.string   :reset_password_token
+      t.string   :reset_password_token, limit: 191
       t.datetime :reset_password_sent_at
 
       t.datetime :remember_created_at
