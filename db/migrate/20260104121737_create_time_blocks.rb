fb0072c933d60f8c2179af93809ab6320768e5c0
Towa0424 <w8d42bed72xbyx@gmail.com>
2026-01-13 23:43:25 +0900

    DB修正

--- db/migrate/20260104121737_create_time_blocks.rb
+++ db/migrate/20260104121737_create_time_blocks.rb
@@ -1,7 +1,7 @@
 class CreateTimeBlocks < ActiveRecord::Migration[7.1]
   def change
     create_table :time_blocks do |t|
-      t.string  :name,       null: false
+      t.string  :name,       null: false, limit: 191
       t.string  :color_code, null: false
       t.boolean :has_cost,   null: false, default: true
       t.boolean :has_sales,  null: false, default: true
