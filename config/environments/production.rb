392e41014e6db37b8f3e4a2e0bc2792f6230095f
Towa0424 <w8d42bed72xbyx@gmail.com>
2026-01-15 06:56:42 +0900

    自動デプロイの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ Rails.application.configure do
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  # config.public_file_server.enabled = false
+  # Serve static files from `public/` when no front-end web server is present.
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Allow the asset pipeline to compile on demand if a precompiled asset is missed.
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
